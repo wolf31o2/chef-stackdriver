3dc3daddef6a55afe531c916d984c421dc698b0c
tkling <tyling@gmail.com>
2014-04-21 10:52:18 -0400

    seeing if long_description is the problem

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       'David Laube'
 maintainer_email 'dave@dlaube.com'
 license          'All rights reserved'
 description      'Installs/Configures stackdriver'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description 'yay' # IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
