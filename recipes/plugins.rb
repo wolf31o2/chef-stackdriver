408406776e3abe4dfc25b12229a54764ed852180
Chris Gianelloni <chris@cask.co>
2017-04-11 13:10:30 -0400

    Massive update
    - Support GCM detection
    - Support PROXY_URL
    - Cleanup rubocop complaints
    - Remove Vagrant testing
    - Cleanup minor syntax bugs
    - Cleanup foodcritic complaints

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -8,35 +8,34 @@
 # All rights reserved
 #
 
-
 node['stackdriver']['plugins']['active'].each do |plugin|
   template "/opt/stackdriver/collectd/etc/collectd.d/#{plugin}.conf" do
     source "#{plugin}.conf.erb"
-    mode "0644"
-    owner "root"
-    group "root"
+    mode '0644'
+    owner 'root'
+    group 'root'
     action :create
   end
-  node['stackdriver']['plugins']["#{plugin}"]['requires'].each do |package|
-    yum_package "#{package}"
+  node['stackdriver']['plugins'][plugin.to_s]['requires'].each do |package|
+    yum_package package.to_s
   end
 end
-
 
 if node['stackdriver']['plugins']['active'].include?('tomcat')
-    include_recipe "stackdriver::jmxtrans"
+  include_recipe 'stackdriver::jmxtrans'
 end
 
-if node['stackdriver']['plugins']['active'].include?('nginx') and node['stackdriver']['plugins']['nginx']['add_nginx_status_endpoint']
-  template "/etc/nginx/conf.d/stackdriver_status.conf" do
-    source "nginx.status.conf.erb"
-    mode "0644"
-    owner "root"
-    group "root"
+if node['stackdriver']['plugins']['active'].include?('nginx') &&
+   node['stackdriver']['plugins']['nginx']['add_nginx_status_endpoint']
+  template '/etc/nginx/conf.d/stackdriver_status.conf' do
+    source 'nginx.status.conf.erb'
+    mode '0644'
+    owner 'root'
+    group 'root'
     action :create
   end
 end
 
-service "stackdriver-agent" do
+service 'stackdriver-agent' do
   action :restart
 end
--- recipes/repo.rb
+++ recipes/repo.rb
@@ -8,17 +8,17 @@
 # All rights reserved
 #
 
-case node[:platform]
-when 'rhel', 'centos', 'amazon'
+case node['platform_family']
+when 'rhel', 'amazon'
 
   # Re-make the yum cache vi command resource
-  execute "create-yum-cache" do
-   command "yum -q makecache"
-   action :nothing
+  execute 'create-yum-cache' do
+    command 'yum -q makecache'
+    action :nothing
   end
 
   # Reload the yum cache using the Chef provider
-  ruby_block "internal-yum-cache-reload" do
+  ruby_block 'internal-yum-cache-reload' do
     block do
       Chef::Provider::Package::Yum::YumCache.instance.reload
     end
@@ -26,27 +26,27 @@ when 'rhel', 'centos', 'amazon'
   end
 
   # Create the StackDriver yum repo file in yum.repos.d
-  cookbook_file "/etc/yum.repos.d/stackdriver.repo" do
+  cookbook_file '/etc/yum.repos.d/stackdriver.repo' do
     source platform?('amazon') ? 'amazonlinux-stackdriver.repo' : 'stackdriver.repo'
-    mode 00644
-    notifies :run, "execute[create-yum-cache]", :immediately
-    notifies :create, "ruby_block[internal-yum-cache-reload]", :immediately
+    mode '0644'
+    notifies :run, 'execute[create-yum-cache]', :immediately
+    notifies :create, 'ruby_block[internal-yum-cache-reload]', :immediately
   end
 
-when 'ubuntu'
+when 'debian'
 
   # Create the StackDriver apt repo list
-  template "/etc/apt/sources.list.d/stackdriver.list" do
-    source "stackdriver.list.apt.erb"
-    mode "0644"
-    owner "root"
-    group "root"
+  template '/etc/apt/sources.list.d/stackdriver.list' do
+    source 'stackdriver.list.apt.erb'
+    mode '0644'
+    owner 'root'
+    group 'root'
     action :create
   end
 
-  #update the local package list
-  execute "stackdriver-apt-get-update" do
-    command "apt-get update"
+  # update the local package list
+  execute 'stackdriver-apt-get-update' do
+    command 'apt-get update'
     action :nothing
   end
 end
